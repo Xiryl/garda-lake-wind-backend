8c0a4e5e86ffcc1392e3d1d2c4dea57be6102830
Fabio <fabio@chiarani.it>
2022-06-23 08:39:29 +0200

    update api for each location

--- src/routes/v1/openapi.js
+++ src/routes/v1/openapi.js
@@ -1,5 +1,12 @@
 const logger = require('../../utils/logger');
 
+const dataNavene = require('../../lib/naveneParser');
+const dataLimone = require('../../lib/limoneParser');
+const dataCvt = require('../../lib/cvtParser');
+const dataRdg = require('../../lib/rdgParser');
+const dataCampione = require('../../lib/campioneParser');
+const dataTorri = require('../../lib/torriParser');
+
 /**
  * [GET] /api/v1/status
  * @description reply with 200 if the server is up
@@ -27,12 +34,112 @@ const route = async (fastify) => {
       .send({ status: true, message: 'up' });
   });
 
-  fastify.get('/limone', statusSchema, async (request, reply) => {
-    logger.debug(`[${process.pid}][${request.id}] Invoking server status`);
-    reply
-      .code(200)
-      .header('Content-Type', 'application/json; charset=utf-8')
-      .send({ status: true, message: 'up' });
+  fastify.get('/limone', async (request, reply) => {
+    logger.debug(`[${process.pid}][${request.id}] Invoking limone`);
+
+    try {
+      const jsonData = await dataLimone.elaborate();
+      reply
+        .code(200)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: true, data: jsonData });
+    } catch (ex) {
+      console.log(ex);
+      reply
+        .code(500)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: false });
+    }
+  });
+
+  fastify.get('/navene', async (request, reply) => {
+    logger.debug(`[${process.pid}][${request.id}] Invoking navene`);
+
+    try {
+      const jsonData = await dataNavene.elaborate();
+      reply
+        .code(200)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: true, data: jsonData });
+    } catch (ex) {
+      console.log(ex);
+      reply
+        .code(500)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: false });
+    }
+  });
+
+  fastify.get('/rivadelgarda', async (request, reply) => {
+    logger.debug(`[${process.pid}][${request.id}] Invoking rdg`);
+
+    try {
+      const jsonData = await dataRdg.elaborate();
+      reply
+        .code(200)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: true, data: jsonData });
+    } catch (ex) {
+      console.log(ex);
+      reply
+        .code(500)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: false });
+    }
+  });
+
+  fastify.get('/torbole', async (request, reply) => {
+    logger.debug(`[${process.pid}][${request.id}] Invoking torbole`);
+
+    try {
+      const jsonData = await dataCvt.elaborate();
+      reply
+        .code(200)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: true, data: jsonData });
+    } catch (ex) {
+      console.log(ex);
+      reply
+        .code(500)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: false });
+    }
+  });
+
+  fastify.get('/campione', async (request, reply) => {
+    logger.debug(`[${process.pid}][${request.id}] Invoking campione`);
+
+    try {
+      const jsonData = await dataCampione.elaborate();
+      reply
+        .code(200)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: true, data: jsonData });
+    } catch (ex) {
+      console.log(ex);
+      reply
+        .code(500)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: false });
+    }
+  });
+
+  fastify.get('/torri', async (request, reply) => {
+    logger.debug(`[${process.pid}][${request.id}] Invoking torri`);
+
+    try {
+      const jsonData = await dataTorri.elaborate();
+      reply
+        .code(200)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: true, data: jsonData });
+    } catch (ex) {
+      console.log(ex);
+      reply
+        .code(500)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ status: false });
+    }
   });
 };
 
